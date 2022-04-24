19ee15ce3b8cebfe0d4d01c3c19b6506c8e815b3
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2022-04-24 17:20:38 +0200

    Update import_data function

--- R/import_data.R
+++ R/import_data.R
@@ -1,19 +1,20 @@
 #' Import Data
 #'
 #' Function to import data from the local MariaDB database.
-#' @param user Character. Your username to connect to the database. No default.
+#' @param user Character. Your username to connect to the database. Defaults to Sys.getenv("DB_USERNAME").
+#' @param password Character. Your password to connect to the database. Defaults to Sys.getenv("DB_PASSWORD"). 
 #' @param db.name Character. The name of the database. Defaults to "opportunities_for_improvement".
 #' @param table.names Character. The name(s) of the table(s) in the database. Defaults to c("swetrau", "fmp", "atgarder", "problem").
-#' @param password Character. Your password to connect to the database. Defaults to keyring::key_get(db.name, user).
-import_data <- function(user,
+import_data <- function(user = Sys.getenv("DB_USERNAME"),
+                        password = Sys.getenv("DB_PASSWORD"),
                         db.name = "opportunities_for_improvement",
-                        table.names = c("swetrau", "fmp", "atgarder", "problem"),
-                        password = keyring::key_get(db.name, user)) {
+                        table.names = c("swetrau", "fmp", "atgarder", "problem")) {
     ## Create connection to the database
     conn <- DBI::dbConnect(drv = RMariaDB::MariaDB(),
                        user = user,
-                       password = keyring::key_get(db.name, user),
+                       password = password,
                        db = db.name)
-    datasets <- lapply(tables.name, function(table.name) dbReadTable(conn = conn, name = table.name))
+    datasets <- lapply(table.names, function(table.name) DBI::dbReadTable(conn = conn, name = table.name))
+    DBI::dbDisconnect(conn)
     return (datasets)
 }

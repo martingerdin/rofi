acb1400b01b4fccff2a8ef42ba0dbc98f9789fca
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2022-05-07 11:57:17 +0200

    Add kvalgranskning to merge_dataset

--- R/merge_data.R
+++ R/merge_data.R
@@ -3,20 +3,20 @@
 #' Merges the different opportunities for improvement datasets into a
 #' single data.frame.
 #' @param datasets A list of data.frames. Has to include the datasets
-#'     "swetrau", "fmp", "atgarder", and "problem". The dataset
-#'     "swetrau" has to include the variables
-#'     "DateTime_ArrivalAtHospital", "PersonIdentity", and
-#'     "TempIdentity". The datasets "fmp" and "problem" have to
+#'     "swetrau", "fmp", "atgarder", "problem", and
+#'     "kvalgranskning2014.2017". The dataset "swetrau" has to include
+#'     the variables "DateTime_ArrivalAtHospital", "PersonIdentity",
+#'     and "TempIdentity". The datasets "fmp" and "problem" have to
 #'     include the variables "Ankomst_te", "Personnummer", and
 #'     "Reservnummer".
 merge_data <- function(datasets) {
-    dataset.names <- c("swetrau", "fmp", "atgarder", "problem")
+    dataset.names <- c("swetrau", "fmp", "atgarder", "problem", "kvalgranskning2014.2017")
     assertthat::assert_that(is.list(datasets))
     assertthat::assert_that(all(dataset.names %in% names(datasets)))
-    assertthat::assert_that(all(c("DateTime_ArrivalAtHospital", "PersonIdentity", "TempIdentity") %in% names(datasets$swetrau)))
-    assertthat::assert_that(all(c("Ankomst_te", "Personnummer", "Reservnummer") %in% names(datasets$fmp)))
-    assertthat::assert_that(all(c("Ankomst_te", "Personnummer", "Reservnummer") %in% names(datasets$problem)))
     attach(datasets)
+    assertthat::assert_that(all(c("DateTime_ArrivalAtHospital", "PersonIdentity", "TempIdentity") %in% names(swetrau)))
+    assertthat::assert_that(all(c("Ankomst_te", "Personnummer", "Reservnummer") %in% names(fmp)))
+    assertthat::assert_that(all(c("Ankomst_te", "Personnummer", "Reservnummer") %in% names(problem)))
     swetrau$arrival <- as.POSIXct(strptime(swetrau$DateTime_ArrivalAtHospital, format = "%Y-%m-%d %H:%M"))
     fmp$arrival <- as.POSIXct(strptime(fmp$Ankomst_te, format = "%Y%m%d %H:%M"))
     problem$arrival <- as.POSIXct(strptime(problem$Ankomst_te, format = "%Y%m%d %H:%M"))

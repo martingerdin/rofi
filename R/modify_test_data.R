6a5ff5e4a5bd179a3fdf786f8819d5a283d54545
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2022-05-15 20:46:23 +0200

    Change test case

--- R/modify_test_data.R
+++ R/modify_test_data.R
@@ -3,6 +3,6 @@
 #' Internal functions used to modify test data
 add_ofi_test_case <- function() {
     ofi.test.data <- rbind(ofi.test.data,
-                           c("Ja", NA, 1, "3", "Yes"))
+                           c("Nej", NA, 2, NA, "No"))
     usethis::use_data(ofi.test.data, internal = TRUE, overwrite = TRUE)
 }

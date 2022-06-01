25dfd2dacb37855457cf772197be6d93cfb1ef72
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-06-02 00:11:46 +0200

    Add arguments to import_data and allow it to read credentials from stdin as well as write them to local .env

--- R/import_data.R
+++ R/import_data.R
@@ -17,12 +17,20 @@
 #'     called DB_PASSWORD, using Sys.getenv("DB_PASSWORD").
 #' @param db.name Character. The name of the database. Defaults to
 #'     "opportunities_for_improvement".
+#' @param test.db.name Character. The name of the test
+#'     database. Defaults to `paste0(db.name, "_scrambled")` and is
+#'     only used if test = TRUE.
 #' @param table.names Character. The name(s) of the table(s) in the
 #'     database. Defaults to c("swetrau", "fmp", "atgarder",
 #'     "problem", "kvalgranskning2014.2017").
 #' @param env.file Character. The name of the file defining the
 #'     environment variables used to connect to the database. Defaults
 #'     to ".env".
+#' @param ask Logical. If TRUE you will be prompted to enter your user
+#'     name and password to access the database, if these are not
+#'     available as environment variables. Defaults to TRUE.
+#' @param test Logical. If TRUE the test, also called scrambled,
+#'     version of the data will be imported. Defaults to FALSE.
 #' @param silent Logical. If FALSE a message is displayed with the
 #'     names of the imported datasets. Defaults to FALSE.
 #' @return A list of data.frames.
@@ -30,28 +38,49 @@
 import_data <- function(user = NULL,
                         password = NULL,
                         db.name = "opportunities_for_improvement",
+                        test.db.name = paste0(db.name, "_scrambled"),
                         table.names = c("swetrau",
                                         "fmp",
                                         "atgarder",
                                         "problem",
                                         "kvalgranskning2014.2017"),
                         env.file = ".env",
+                        ask = TRUE,
+                        test = FALSE,
                         silent = FALSE) {
     ## Check arguments
-    for (argument in list(user, password, db.name))
+    for (argument in list(user, password, db.name, test.db.name))
         assertthat::assert_that(is.null(argument) || (is.character(argument) & length(argument) == 1))
     assertthat::assert_that(is.character(table.names))
-    assertthat::assert_that(is.logical(silent) & length(silent) == 1)
+    for (argument in list(silent, test))
+        assertthat::assert_that(is.logical(argument) & length(argument) == 1)
     ## Load user and password from environment variables unless passed in
-    if (is.null(user) | is.null(password))
+    if ((is.null(user) | is.null(password)) & file.exists(env.file))
         dotenv::load_dot_env(file = env.file)
     if (is.null(user)) {
         user <- Sys.getenv("DB_USERNAME")
-        if (user == "") stop ("No environment variable called DB_USERNAME was found.")
+        if (user == "" & interactive() & ask) {
+            user <- askpass::askpass("We did not find your username for accessing the database in the environment variable called DB_USERNAME. Please enter it here:")
+            message ("Do you want to save your username in a local .env file for future access? Please remember to add this .env file to .gitignore. (Y/N)")
+            answer <- readLines(n = 1)
+            if (tolower(answer) == "y")
+                write(paste0("DB_USERNAME=", user), ".env", append = TRUE)
+            
+        }
+        if (user == "" & !ask)
+            stop ("No environment variable called DB_USERNAME was found.")
     }
     if (is.null(password)) {
         password <- Sys.getenv("DB_PASSWORD")
-        if (password == "") stop ("No environment variable called DB_PASSWORD was found.")
+        if (password == "" & interactive() & ask) {
+            password <- askpass::askpass("We did not find your password for accessing the database in the environment variable called DB_PASSWORD. Please enter it here:")
+            message ("Do you want to save your password in a local .env file for future access? Please remember to add this .env file to .gitignore.")
+            answer <- readLines(n = 1)
+            if (tolower(answer) == "y")
+                write(paste0("DB_PASSWORD=", password), ".env", append = TRUE)
+        }
+        if (password == "" & !ask)
+            stop ("No environment variable called DB_PASSWORD was found.")
     }
     ## Create connection to the database
     conn <- DBI::dbConnect(drv = RMariaDB::MariaDB(),

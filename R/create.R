45162cda61f4b129c413d1f6618b2e4cee40d9cb
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2022-08-12 08:30:22 +0200

    Add wrapper for noacsr::create

--- R/create.R
+++ R/create.R
@@ -1,15 +1,7 @@
 #' Create and Scaffold an Opportunities for Improvement Project
 #' 
-#' Run this function to initiate an opportunities for improvement project and 
-#' scaffold an appropriate folder structure. 
-#' @param name Character. The name of the project. No default. 
-#' @param description Character. A short, one sentence, description of the 
-#' project. This is added to the DESCRIPTION file. No default. 
-create <- function(name, description) {
-  ## Check arguments
-  for (argument in list(name, description)) 
-    assertthat::assert_that(is.character(argument) & length(argument) == 1)
-  ## Use devtools::create to initiate a R package folder structure
-  usethis::create_package(name)
-  
+#' See [noacsr::create()]
+#' @export
+create <- function(name) {
+    noacsr::create(name)
 }

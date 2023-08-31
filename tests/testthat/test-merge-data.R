860a3085721b537465d66d4a15bda4f16bbad64f
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2023-08-31 09:54:43 +0200

    Update merge data test to respect environment

--- tests/testthat/test-merge-data.R
+++ tests/testthat/test-merge-data.R
@@ -1,5 +1,7 @@
 test_that("merge_data returns the expected data.frame", {
-    datasets <- import_data(silent = TRUE)
+    ENV <- Sys.getenv("ENV")
+    test <- if (ENV == "test") TRUE else FALSE
+    datasets <- import_data(silent = TRUE, test = test)
     merged.data <- merge_data(datasets)
     expect_s3_class(merged.data, "data.frame")
     expect_equal(names(merged.data), get_data_names())
